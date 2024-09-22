89af9a9bee268c3cc8f0b506b642f5c80b45a567
Mate Borsy <mateborsy@Mac.home>
2024-09-22 19:11:01 +0200

    create layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,8 +2,44 @@
 function App() {
 
   return (
-    <>
-    </>
+    <div className="">
+      <div className="grid grid-cols-[20%_60%_20%] grid-rows-[10vh_10vh_80vh]  min-h-full">
+        <div className="border p-4">
+          <h2 className="text-lg font-bold">1</h2>
+        </div>
+        <div className="border p-4">
+          <h2 className="text-lg font-bold">2</h2>
+        </div>
+        <div className="border p-4">
+          <h2 className="text-lg font-bold">3</h2>
+        </div>
+
+        <div className="border p-4">
+          <h2 className="text-lg font-bold">1</h2>
+        </div>
+        <div className="border p-4">
+          <h2 className="text-lg font-bold">2</h2>
+        </div>
+        <div className="border p-4">
+          <h2 className="text-lg font-bold">3</h2>
+        </div>
+
+        <div className="border p-4">
+          <h2 className="text-lg font-bold">1</h2>
+        </div>
+        <div className="border p-4">
+          <h2 className="text-lg font-bold">2</h2>
+        </div>
+        <div className="border p-4">
+          <h2 className="text-lg font-bold">3</h2>
+        </div>
+      </div>
+    </div>
+
+
+
+
+
   )
 }
 
