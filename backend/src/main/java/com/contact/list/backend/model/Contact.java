31f514258f00451629979e57f5c1b4ab0ce22aff
Mate Borsy <mateborsy@Mac.home>
2024-09-22 13:34:52 +0200

    added controllers for user,contact - contactEntity jsonBackReference, userentity JsonManagedReference to prevent cycle

--- backend/src/main/java/com/contact/list/backend/model/Contact.java
+++ backend/src/main/java/com/contact/list/backend/model/Contact.java
@@ -1,21 +1,27 @@
 package com.contact.list.backend.model;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 
 @Getter
 @Setter
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
 @SuperBuilder
 @Table(name = "contacts")
 public class Contact extends Person {
 
     @ManyToOne
     @JoinColumn(name = "user_id", nullable = false)
+    @JsonBackReference
     private User user;
 }
